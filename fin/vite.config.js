1a30df471cfa75feb7569fcda63df70e450ea9b1
Abinandan G <152169567+gabinandanui@users.noreply.github.com>
2025-11-25 14:48:21 +0000

    vercel deployment related changes and data cleanedup

--- fin/vite.config.js
+++ fin/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../dist'
+  },
   plugins: [react()],
 })
